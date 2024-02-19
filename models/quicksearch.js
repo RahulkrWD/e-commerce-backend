8798fadcdd9668e3734bde65ca27894bab9b5642
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-02-19 17:44:43 +0530

    update

---
+++ models/quicksearch.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+const quickSearchSchema = new mongoose.Schema({});
+module.exports = mongoose.model("quicksearchs", quickSearchSchema);
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const category = require("../controllers/ProductController/category");
 const router = express.Router();
+const quicksearch = require("../controllers/ProductController/quickSearch");
 
 router.get("/category", category);
+
+router.get("/quicksearch", quicksearch);
+router.get("/quicksearch/:id", quicksearch);
 module.exports = router;
