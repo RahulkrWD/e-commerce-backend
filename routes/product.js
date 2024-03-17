2fa202ee4de0f8b3e9a98b9f5540da56571331c2
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-03-17 16:12:33 +0530

    delete category controller and convert backend data to frontend

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,6 @@
 const express = require("express");
-const category = require("../controllers/ProductController/category");
 const router = express.Router();
 const productController = require("../controllers/ProductController/ProductController");
-
-// category
-router.get("/category", category.category);
-router.post("/add/category", category.addCategory);
-router.delete("/delete/category", category.deleteCategory);
 
 // All product
 router.get("/product", productController.product);
